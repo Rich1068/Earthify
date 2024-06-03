abda855e67cd199077cf8388b3c358496f87b256
Angelika Mae Lopez <lopez.angelikamae@auf.edu.ph>
2024-06-03 22:17:50 +0800

    updated color scheme

---
+++ Earthify/Earthify/Model/ActionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Earthify.Model
+{
+    public class ActionModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Impact_Level { get; set; }
+        public string Impact_Description { get; set; }
+        public string Frequency { get; set; }
+    }
+}
---
+++ Earthify/Earthify/ViewModel/ActionViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Earthify.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace Earthify.ViewModel
+{
+    public class ActionViewModel
+    {
+        private Services.DatabaseContext getContext()
+        {
+            return new Services.DatabaseContext();
+        }
+        public int InsertAction(ActionModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Action.Add(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+        public async Task<List<ActionModel>> GetAllAction()
+        {
+            var _dbContext = getContext();
+            var res = await _dbContext.Action.ToListAsync();
+            return res;
+        }
+        public async Task<int> UpdateAction(ActionModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Action.Update(obj);
+            int c = await _dbContext.SaveChangesAsync();
+            return c;
+        }
+
+        public int DeleteAction(ActionModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Action.Remove(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+    }
+}
