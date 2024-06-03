6bccdf7264c7abc0afc9967d020391b130a781ad
Angelika Mae Lopez <lopez.angelikamae@auf.edu.ph>
2024-06-03 22:48:13 +0800

    color scheme

---
+++ Earthify/Earthify/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Earthify
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new View.ShowActionPage())
+            {
+                BarBackgroundColor = Color.FromHex("#254336"),
+                BackgroundColor = Color.FromHex("#DAD3BE")
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Earthify/Earthify/View/ActionView.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Earthify.ViewModel;
+using Earthify.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System.Xml.Linq;
+
+namespace Earthify.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ActionView : ContentPage
+    {
+        ActionViewModel _viewModel;
+        bool _isUpdate;
+        int actionID;
+        public ActionView()
+        {
+            InitializeComponent();
+            _viewModel = new ActionViewModel();
+            _isUpdate = false;
+        }
+        public ActionView(ActionModel obj)
+        {
+            InitializeComponent();
+            _viewModel = new ActionViewModel();
+            if (obj != null)
+            {
+                actionID = obj.Id;
+                txtDescription.Text = obj.Description;
+                txtCategory.Text = obj.Category;
+                txtImpactlvl.Text = obj.Impact_Level;
+                txtImpactdesc.Text = obj.Impact_Description;
+                txtFrequency.Text = obj.Frequency;
+                _isUpdate = true;
+            }
+        }
+        private async void btnSaveUpdate_Clicked(object sender, EventArgs e)
+        {
+            ActionModel obj = new ActionModel();
+            obj.Description = txtDescription.Text;
+            obj.Category = txtCategory.Text;
+            obj.Impact_Level = txtImpactlvl.Text;
+            obj.Impact_Description = txtImpactdesc.Text;
+            obj.Frequency = txtFrequency.Text;
+
+            if (_isUpdate)
+            {
+                obj.Id = actionID;
+                await _viewModel.UpdateAction(obj);
+
+            }
+            else
+            {
+                _viewModel.InsertAction(obj);
+            }
+
+
+            await this.Navigation.PopAsync();
+        }
+    }
+}
