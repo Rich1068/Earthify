2f18558a348eb427b3e088e89c050fe8c6efca3f
kovlor13 <127574325+kovlor13@users.noreply.github.com>
2024-06-05 15:04:37 +0800

    Courusell Page

--- Earthify/Earthify/MainPage.xaml.cs
+++ Earthify/Earthify/MainPage.xaml.cs
@@ -18,9 +18,9 @@ namespace Earthify
 
             List<ImageModel> images = new List<ImageModel>()
             {
-                new ImageModel(){Title="Image 1",Url="https://i.ibb.co/PFtB8Mh/1.png"},
-                new ImageModel(){Title="Image 2",Url="https://i.ibb.co/FXTbT34/2.png"},
-                new ImageModel(){Title="Image 3",Url="https://i.ibb.co/ngSt0Z8/3.png"}
+                new ImageModel(){Title="Image 1",Url="https://i.postimg.cc/pVq52qVH/image.png"},
+                new ImageModel(){Title="Image 2",Url="https://i.postimg.cc/B6yWgrc3/image.png"},
+                new ImageModel(){Title="Image 3",Url="https://i.postimg.cc/zfV0jVdL/image.png"}
             };
 
             Carousel.ItemsSource = images;
